ab4860bcbcd927b8a56971daa7ba9d9b1d28c0cd
Mohamed Fofana <mohamed.fofana63@yahoo.com>
2023-08-16 01:56:48 -0400

    account model, controller and db repo setup

---
+++ src/main/java/com/odds/accountservice/controller.java
@@ -0,0 +1,23 @@
+package com.odds.accountservice;
+
+import com.odds.accountservice.model.Account;
+import com.odds.accountservice.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
+public class controller {
+
+    @Autowired
+    AccountRepository repository;
+
+    @GetMapping("/all")
+    public List<Account> getAllAccounts() {
+        return repository.findAll();
+    }
+}
