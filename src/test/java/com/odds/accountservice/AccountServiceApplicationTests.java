30efd194da9a31de64772b21941ab8fef9efc08b
Mohamed Fofana <mohamed.fofana63@yahoo.com>
2023-08-17 00:17:03 -0400

    error fix

--- src/test/java/com/odds/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/com/odds/accountservice/AccountServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.odds.accountservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class AccountServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
